d8b188c1ec90058313a2a0eafd11a941a7648b14
Vu Hoang Quan <vhquan@apcs.vn>
2019-07-08 22:53:26 +0700

    first commit

---
+++ data/Kinetics/read.py
@@ -0,0 +1,8 @@
+import numpy as np
+data = np.load('train_data.npy')
+#d = data.shape
+print(str(len(data)))
+'''for j in range(36):
+    print(str(data[0][2][1][j][0]) + '\n')'''
+print(str(data[0][2][1]) + '\n')
+#print(str(d))
